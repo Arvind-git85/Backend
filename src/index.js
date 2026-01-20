e72a426d6506465527af6358a08060701ba99fac
Arvind <ka840895@gmail.com>
2026-01-20 20:15:09 +0530

    Utils code

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ import dotenv from 'dotenv'
 dotenv.config({ path: './.env' })
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`sever is running at port: ${process.env.PORT}`);
+    })
+})
+.catch( (e) => {
+    console.log("MongoDB connection error", e)
+})
